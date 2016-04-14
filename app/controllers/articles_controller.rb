1e1a51262a96797c5c239ebf2a1fd8bbc3255e01
Brian Chen <380024675@qq.com>
2016-04-14 23:36:37 +0800

    more

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
    def index
      @articles = Article.all
    end
@@ -31,6 +32,12 @@ def update
     else
     render 'edit'
   end
+end
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  
+  redirect_to articles_path
 end
 
 private
